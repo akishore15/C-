b2fbc94875ec99d3cc865ffa6ae937b6e11aa677
hitor- 1 <154870205+akishore15@users.noreply.github.com>
2025-01-07 21:59:25 -0800

    Add files via upload

---
+++ interpreter.js
@@ -0,0 +1,213 @@
+let cmd = prompt();
+let str = string;
+let a = prompt("Intname 1: ");
+let b = prompt("Intname 2: ");
+var cout = function(str){
+    console.log(str);
+}
+var input = function(str){
+    return prompt(str);
+}
+var add = function(a, b){
+    return a + b;
+}
+var subtract = function(a, b){
+    return a - b;
+}
+var expon = function(a, b){
+    return a ** b;
+}
+var div = function(a, b){
+    return a / b;
+}
+var mult = function(a, b){
+    return a * b;
+}
+var mod = function(a, b){
+    return a % b;
+}
+var int = function(a){
+    return parseInt(a);
+}
+var float = function(a){
+    return parseFloat(a);
+}
+var astr = function(a){
+    return a.toString();
+}
+var bstr = function(b){
+    return b.toString();
+}
+var bool = function(a){
+    return Boolean(a);
+}
+var eq = function(a, b){
+    return a == b;
+}
+var neq = function(a, b){
+    return a != b;
+}
+var gt = function(a, b){
+    return a > b;
+}
+var lt = function(a, b){
+    return a < b;
+}
+var gte = function(a, b){
+    return a >= b;
+}
+var lte = function(a, b){
+    return a <= b;
+}
+var and = function(a, b){
+    return a && b;
+}
+var or = function(a, b){
+    return a || b;
+}
+var not = function(a){
+    return !a;
+}
+var xor = function(a, b){
+    return a ^ b;
+}
+var lshift = function(a, b){
+    return a << b;
+}
+var rshift = function(a, b){
+    return a >> b;
+}
+var urshift = function(a, b){
+    return a >>> b;
+}
+var band = function(a, b){
+    return a & b;
+}
+var bor = function(a, b){
+    return a | b;
+}
+var bxor = function(a, b){
+    return a ^ b;
+}
+var bnot = function(a){
+    return ~a;
+}
+var bneg = function(a){
+    return -a;
+}
+var bpos = function(a){
+    return +a;
+}
+var binc = function(a){
+    return a++;
+}
+var bdec = function(a){
+    return a--;
+}
+var babs = function(a){
+    return Math.abs(a);
+}
+var bceil = function(a){
+    return Math.ceil(a);
+}
+var bfloor = function(a){
+    return Math.floor(a);
+}
+var bround = function(a){
+    return Math.round(a);
+}
+var btrunc = function(a){
+    return Math.trunc(a);
+}
+var bsign = function(a){
+    return Math.sign(a);
+}
+var bmax = function(a, b){
+    return Math.max(a, b);
+}
+var bmin = function(a, b){
+    return Math.min(a, b);
+}
+var bpow = function(a, b){
+    return Math.pow(a, b);
+}
+var pytheorem = function(a, b){
+    c ** 2 == a ** 2 + b ** 2;
+    return c;
+}
+var acos = function(a){
+    return Math.cos(a);
+}
+var asin = function(a){
+    return Math.sin(a);
+}
+var atan = function(a){
+    return Math.tan(a);
+}
+var acosh = function(a){
+    return Math.cosh(a);
+}
+var asinh = function(a){
+    return Math.sinh(a);
+}
+var atanh = function(a){
+    return Math.tanh(a);
+}
+var acosh = function(a){
+    return Math.acosh(a);
+}
+var asinh = function(a){
+    return Math.asinh(a);
+}
+var atanh = function(a){
+    return Math.atanh(a);
+}
+var log = function(a){
+    return Math.log(a);
+}
+var log10 = function(a){
+    return Math.log10(a);
+}
+var log2 = function(a){
+    return Math.log2(a);
+}
+var exp = function(a){
+    return Math.exp(a);
+}
+var sqrt = function(a){
+    return Math.sqrt(a);
+}
+var cbrt = function(a){
+    return Math.cbrt(a);
+}
+var hypot = function(a, b){
+    return Math.hypot(a, b);
+}
+var random = function(){
+    return Math.random();
+}
+var seed = function(a){
+    return Math.seed(a);
+}
+var floor = function(a){
+    return Math.floor(a);
+}
+var ceil = function(a){
+    return Math.ceil(a);
+}
+var round = function(a){
+    return Math.round(a);
+}
+var trunc = function(a){
+    return Math.trunc(a);
+}
+var sign = function(a){
+    return Math.sign(a);
+}
+var max = function(a, b){
+    return Math.max(a, b);
+}
+var min = function(a, b){
+    return Math.min(a, b);
+}
+
